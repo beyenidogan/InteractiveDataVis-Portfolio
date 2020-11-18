0eab589af87d758387065de7b0465cdf903add74
Berna <bernay@Bilgisayarim.local>
2020-11-18 03:35:57 +0300

    Updated transitions and tooltip format

--- Exploratory-Filming-in-NYC/Line.js
+++ Exploratory-Filming-in-NYC/Line.js
@@ -40,6 +40,7 @@ class Line {
             .range([this.margins.left, this.width - this.margins.right])
         console.log(d3.extent(filteredData,d=>d.Month))
 
+
         const yScale = d3
             .scaleLinear()
             .domain([0,d3.max(showbyData, d => d.Events)])
@@ -55,9 +56,18 @@ class Line {
                 .attr("class", "x-axis")
                 .call(d3.axisBottom(xScale)
                     .ticks(d3.timeMonth)
-                    .tickSize(5,0) 
+                    .tickSize(9,0) 
                     .ticks(5)
                     .tickFormat(d3.timeFormat("%Y")))
+        
+        const xAxis2 = this.svg.append("g")
+                .attr("transform", "translate(0," + (this.height-this.margins.bottom) + ")")
+                .attr("class", "x-axis2")
+                .call(d3.axisBottom(xScale)
+                    .ticks(d3.timeMonth)
+                    .tickSize(5,0) 
+                    .tickFormat(""))       
+
         let yAxis = this.svg.append("g")
                 .attr("transform", "translate(40," + (-this.margins.bottom) + ")")
                 .attr("class", "y-axis")
@@ -67,12 +77,7 @@ class Line {
  
 
         console.log(state)
-/*         yScale.domain([0, d3.max(filteredData, d => d.Events)]);
 
-        d3.select("g.y-axis")
-        .transition()
-        .duration(1000)
-        .call(yAxis.scale(yScale)); */
         console.log("filtered" ,filteredData)
 
         const dot = this.svg
@@ -83,6 +88,8 @@ class Line {
                 enter
                     .append("g")
                     .attr("class", "dot")
+                    .attr("cx", d => xScale(d.Month))
+                    .attr("cy", this.height) 
                     .call(enter => enter.append("circle")),
                 update => update,
                 exit =>
@@ -90,7 +97,7 @@ class Line {
                     // exit selections -- all the `.dot` element that no longer match to HTML elements
                     exit
                         .transition()
-                        .duration(500)
+                        .duration(100)
                         .remove()
                     )
                 ) 
@@ -98,9 +105,9 @@ class Line {
 
         dot.select("circle")
         .transition()
-        .duration(this.duration)
+        .duration(this.duration/10) 
         .attr("transform", "translate(0," + (-this.margins.bottom) + ")")
-        .attr("r", 6)
+        .attr("r", 2)
         .attr("cx", d => xScale(d.Month))
         .attr("cy", d => yScale(d.Events)) // initial value - to be transitioned
         .attr("fill", "#DDDDDD")
@@ -160,8 +167,9 @@ class Line {
             ) 
             .call(selection =>
                 selection
-                  .transition() // sets the transition on the 'Enter' + 'Update' selections together.
-                  .duration(1000)
+                .transition() // sets the transition on the 'Enter' + 'Update' selections together.
+/*                   .duration(this.duration) */
+                .delay((d,i) => 500 * i)
                   .attr("opacity", 1)
                   .attr("fill","none")
                   .attr("d", lineFunc)
