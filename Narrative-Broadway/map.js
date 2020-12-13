bfc4d4e09e6fb11527dc28e73e671fb86212788b
Berna <bernay@Bilgisayarim.local>
2020-12-13 03:19:57 +0300

    Started part 2

--- Narrative-Broadway/map.js
+++ Narrative-Broadway/map.js
@@ -13,11 +13,6 @@ export function Map() {
   let state = {
     geojson: null,
     theaters: null,
-    hover: {
-      latitude: null,
-      longitude: null,
-      theater: null,
-    },
   };
 
   /**
@@ -25,7 +20,7 @@ export function Map() {
    * Using a Promise.all([]), we can load more than one dataset at a time
    * */
   Promise.all([
-    d3.json("./data/manhattan3.geojson"),
+    d3.json("./data/manhattan4.geojson"),
     d3.csv("./data/Theaters.csv", d3.autoType), 
   ]).then(([geojson, theaters]) => {
     state.geojson= geojson;
@@ -58,10 +53,11 @@ export function Map() {
       .join("path")
       .attr("d", path)
       .attr("class", "regions")
-      .attr("stroke","rgba(256,256,256,0.9)")
-      .attr("fill", d => d.properties.name==="Central Park"?"rgba(256,256,256,0.5)":"rgba(256,256,256,0.7)")
+      .attr("stroke","rgba(256,256,256,0.7)")
+      .attr("fill", d => d.properties.name==="Central Park"?"rgba(256,256,256,0.3)":"rgba(256,256,256,0.5)")
 
-      
+
+
       draw()
   }
 
@@ -72,36 +68,44 @@ export function Map() {
   function draw() {
     const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
     console.log(state.theaters)
+
+    let rScale=d3.scaleSqrt()
+    .domain(d3.extent(state.theaters, d => d.Capacity))
+    .range([5,20])
+
+
     svg
     .selectAll(".circle")
     .data(state.theaters,d=>`${d.No}_${d.Name}`)
     .join("circle")
     .attr("class","dot")
     .attr("r", 1)
-    .attr("fill", d => d.Type==="Broadway"?"red":"blue")
-    .attr("opacity",0.6)
+    .attr("fill", d => d.Type==="Broadway"?"#f53670":"blue")
+    .attr("fill-opacity",0.3)
+    .attr("stroke",d => d.Type==="Broadway"?"#f53670":"blue")
     .attr("transform", d => {
       const [x, y] = projection([d.Longitude, d.Latitude]);
       return `translate(${x}, ${y})`;
     })
-    .attr ("r", d => d.Capacity/200)
+    .attr ("r", d => rScale(d.Capacity))
     .on("click", d => {
       d3.select(".dot")
-        .attr("r", 3)
       d3.select(".img").remove()
+      d3.select(".title").remove()
       d3.select("#part1-tooltip")
           .append("div")
           .attr("class", "title")
-          .html('<i><b><p style="font-size: 25px; line-height: 26px;">' 
-                + d.Name + '</i></b> &nbsp(' + d.Capacity + ') ' + '</p>') 
-          .attr('class', 'img')
-          .html('<img src="' + d.ImageLink+ '">')
+          .html('<b><p style="font-size: 20px; line-height: 26px;">' 
+                + d.Name + '</b> ' + '</p> &nbsp') 
+           .attr('class', 'img')
+           .append("div")
+          .html('<img src="' + d.ImageLink+ '" style="max-height:300px; max-width:300px width:auto">') 
           .append("div")
           .attr('class', 'subtitle')
-          .html('<i><b><p style="font-size: 25px; line-height: 26px;">' 
-                + d.Name + '</i></b> &nbsp(' + d.Capacity + ') ' + '</p>' 
+          .html('<p style="font-size: 14px; color:#5F5F5f; line-height: 50px;"> ' 
+                 + d.Address + '</p>' +' &nbsp'
                 + '<p style="font-size: 25px; line-height: 26px;">' 
-                + d.Address + '</p> <p style="color:grey; font-size: 15px; line-height: 16px;">' 
+                + d.Capacity + '</p> <p style="color:grey; font-size: 15px; line-height: 16px;">' 
                 + d.Year + ', &nbsp' + d.NotableShows 
                 + '</p>' + '<p>' + d.OlderNames + ' has ' 
                 + d.Type + ' painting(s) in the collection.</p>')
@@ -109,6 +113,7 @@ export function Map() {
           .attr('class', 'button-container')
           .html('<button id="learn-more-button" class="filter-buttons"><a style="text-decoration: none;" href=' + d.URL + 'target="_new">About the Work</a></button>')
     })
+    
 
     /* svg.selectAll('text')
       .data([state.theaters])
--- Narrative-Broadway/timeline.js
+++ Narrative-Broadway/timeline.js
@@ -4,22 +4,25 @@ export function Timeline() {
   * */
   const margin = { top: 20, bottom: 50, left: 60, right: 40 };
   let svg;
+  const width = window.innerWidth * 0.6,
+  height = window.innerHeight * 0.8
 
   /**
    * APPLICATION STATE
    * */
   let state = {
-    data: null
+    shows: null,
   };
 
   /**
    * LOAD DATA
    * Using a Promise.all([]), we can load more than one dataset at a time
    * */
-  Promise.all([
-    d3.csv("./data/data3.csv"), 
-  ]).then(([data]) => {
-    state.data = data;
+d3.csv("./data/Longest_Running_Shows_v2020-06-02.csv", d3.autoType) 
+  .then(raw_data => {
+    console.log("raw_data", raw_data);
+//    filter;
+    state.shows=raw_data;
     console.log("state: ", state);
     init();
   });
@@ -30,11 +33,35 @@ export function Timeline() {
    * */
   function init() {
 
-    svg = d3
-      .select("#d3-container-3")
-      .append("svg")
+    xScale = d3
+    .scaleTime()
+    .domain([d3.min(state.shows, d => d.OpeningDate),d3.max(state.data, d => d.ClosingDate)])
+    .range([margin.left, width - margin.right])
 
-    draw(); // calls the draw function
+    yScale = d3
+    .scaleBand()
+    .domain(d3.range(state.shows.length))
+    .range([height - margin.bottom, margin.top]);
+  console.log(state.shows.length)
+  svg = d3
+      .select("#part2-timeline")
+      .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  svg
+      .selectAll(".regions")
+      // all of the features of the geojson, meaning all the states as individuals
+      .data(state.geojson.features)
+      .join("path")
+      .attr("d", path)
+      .attr("class", "regions")
+      .attr("stroke","rgba(256,256,256,0.7)")
+      .attr("fill", d => d.properties.name==="Central Park"?"rgba(256,256,256,0.3)":"rgba(256,256,256,0.5)")
+
+
+
+      draw()
   }
 
   /**
@@ -42,16 +69,61 @@ export function Timeline() {
    * we call this everytime there is an update to the data/state
    * */
   function draw() {
+    const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+    console.log(state.theaters)
 
-    svg.style('background-color', 'lightcoral')
+    let rScale=d3.scaleSqrt()
+    .domain(d3.extent(state.theaters, d => d.Capacity))
+    .range([5,20])
 
-    svg.selectAll('text')
-      .data(state.data)
+
+    svg
+    .selectAll(".circle")
+    .data(state.theaters,d=>`${d.No}_${d.Name}`)
+    .join("circle")
+    .attr("class","dot")
+    .attr("r", 1)
+    .attr("fill", d => d.Type==="Broadway"?"#f53670":"blue")
+    .attr("fill-opacity",0.3)
+    .attr("stroke",d => d.Type==="Broadway"?"#f53670":"blue")
+    .attr("transform", d => {
+      const [x, y] = projection([d.Longitude, d.Latitude]);
+      return `translate(${x}, ${y})`;
+    })
+    .attr ("r", d => rScale(d.Capacity))
+    .on("click", d => {
+      d3.select(".dot")
+      d3.select(".img").remove()
+      d3.select(".title").remove()
+      d3.select("#part1-tooltip")
+          .append("div")
+          .attr("class", "title")
+          .html('<b><p style="font-size: 20px; line-height: 26px;">' 
+                + d.Name + '</b> ' + '</p> &nbsp') 
+           .attr('class', 'img')
+           .append("div")
+          .html('<img src="' + d.ImageLink+ '" style="max-height:200px; width:auto">') 
+          .append("div")
+          .attr('class', 'subtitle')
+          .html('<p style="font-size: 14px; color:#5F5F5f; line-height: 26px;"> ' 
+                 + d.Address + '</p>' +' &nbsp'
+                + '<p style="font-size: 25px; line-height: 26px;">' 
+                + d.Capacity + '</p> <p style="color:grey; font-size: 15px; line-height: 16px;">' 
+                + d.Year + ', &nbsp' + d.NotableShows 
+                + '</p>' + '<p>' + d.OlderNames + ' has ' 
+                + d.Type + ' painting(s) in the collection.</p>')
+          .append("div")
+          .attr('class', 'button-container')
+          .html('<button id="learn-more-button" class="filter-buttons"><a style="text-decoration: none;" href=' + d.URL + 'target="_new">About the Work</a></button>')
+    })
+
+    /* svg.selectAll('text')
+      .data([state.theaters])
       .join('text')
       .attr('dx', '50%')
       .attr('dy', '50%')
       .style('text-anchor', 'middle')
-      .text(d => `hello I am mrs SVG number ${d.data}`)
+      .text(d => `${d.data}`) */
 
   }
 }
