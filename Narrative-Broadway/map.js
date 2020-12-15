1b438feb0d76add6b05b8c9f192d31834da044d2
beyenidogan <yenidoganberna@gmail.com>
2020-12-16 02:37:29 +0300

    Format Changes

--- Narrative-Broadway/map.js
+++ Narrative-Broadway/map.js
@@ -3,9 +3,9 @@ export function Map() {
   * CONSTANTS AND GLOBALS
   * */
   const margin = { top: 20, bottom: 50, left: 60, right: 40 };
-  let svg;
+  let svg,projection,maparea,path;
   const width = window.innerWidth * 0.4,
-  height = window.innerHeight * 0.8
+  height = window.innerHeight * 0.9;
 
   /**
    * APPLICATION STATE
@@ -17,14 +17,12 @@ export function Map() {
 
   /**
    * LOAD DATA
-   * Using a Promise.all([]), we can load more than one dataset at a time
    * */
   Promise.all([
-    d3.json("./data/manhattan4.geojson"),
+    d3.json("./data/manhattan3.geojson"),
     d3.csv("./data/Theaters.csv", d3.autoType), 
   ]).then(([geojson, theaters]) => {
     state.geojson= geojson;
-//    filter;
     state.theaters=theaters;
     console.log("state: ", state);
     init();
@@ -36,28 +34,51 @@ export function Map() {
    * */
   function init() {
 
-//ppp
-///ssss
-  const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
-  const path = d3.geoPath().projection(projection);
-  
-  svg = d3
+    projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+    path = d3.geoPath().projection(projection);
+    
+
+  //Create svg
+    svg = d3
       .select("#part1-map")
       .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+      .attr("width", width)
+      .attr("height", height)
+/*        .call(d3.zoom()
+          .on("zoom",  
+            function () {
+            svg.attr("transform", d3.event.transform)
+            }
+          )
+        )  */
 
-  svg
+  //Create svg for clipping
+    const clip = svg
+      .append("defs")
+      .append("svg:clipPath")
+      .attr("id", "clip")
+      .append("svg:rect")
+      .attr("width", width )
+      .attr("height", height )
+      .attr("x", 0)
+      .attr("y", 140);
+
+  //Now create the clipped svg to use to create the map
+    maparea = svg.append('g')
+      .attr("clip-path", "url(#clip)")
+      .attr("transform","translate (0,-120)")
+
+  // Drawing the map
+    maparea
       .selectAll(".regions")
-      // all of the features of the geojson, meaning all the states as individuals
       .data(state.geojson.features)
       .join("path")
       .attr("d", path)
       .attr("class", "regions")
       .attr("stroke","rgba(256,256,256,0.7)")
       .attr("fill", d => d.properties.name==="Central Park"?"rgba(256,256,256,0.3)":"rgba(256,256,256,0.5)")
-
-      draw()
+  
+    draw()
   }
 
   /**
@@ -65,88 +86,80 @@ export function Map() {
    * we call this everytime there is an update to the data/state
    * */
   function draw() {
-    const projection = d3.geoAlbersUsa().fitSize([width, height], state.geojson);
+    
     console.log(state.theaters)
 
     let rScale=d3.scaleSqrt()
-    .domain(d3.extent(state.theaters, d => d.Capacity))
-    .range([5,20])
+      .domain(d3.extent(state.theaters, d => d.Capacity))
+      .range([5,20])
 
     let formatNumber = d3.format(",")
 
-    svg
-    .selectAll(".circle")
-    .data(state.theaters,d=>`${d.No}_${d.Name}`)
-    .join("circle")
-    .attr("class","dot")
-    .attr("r", 4)
-    .attr("fill", d => d.Type==="Broadway"?"#DC756D":"#017a96")
-    .attr("fill-opacity",0.5)
-    .attr("stroke-width","1")
-    .attr("stroke",d => d.Type==="Broadway"?"#974e49":"#026075")
-    .attr("transform", d => {
-      const [x, y] = projection([d.Longitude, d.Latitude]);
-      return `translate(${x}, ${y})`;
-    })
-    .attr ("r", d => rScale(d.Capacity))
-    .on("mouseover", function(d) {                                                            
-      console.log(d3.event.pageX)
-/*       d3.select("#tooltip")
-              .style("left", (d3.event.pageX-100)  + "px")
-              .style("top", d3.event.pageY + "px")						
-              .select("#events")
-              .text(d.Events)
-              .style("fill", "white") */
-     // bars.style("fill", "white")
-      d3.select(this).transition()
-        .duration('50')
-        .attr('fill-opacity', '1')
-        .attr("stroke-width","1.5")
-        .attr ("r", d => rScale(d.Capacity)*1.3)
-    })
-    .on("mouseleave", function(d) {
-      d3.select(this).transition()
-          .duration('50')
-          .attr('fill-opacity', '0.5')
-          .attr("stroke-width","1")
-          .attr ("r", d => rScale(d.Capacity))
-          
-    })  
-    .on("click", d => {
-      console.log(d)
-    d3.select("#part1-tooltip")					
-      .select("#tooltipheader")
-      .text(d.Name)  
-    d3.select("#type")
- //     .append(".text")
-      .text(d.Type.toUpperCase())  
-      .style("color", d.Type==="Broadway"?"#DC756D":"#017a96")  
-    d3.select("#image-theater")
-      .html('<img src="' + d.ImageLink+ '" style="max-height:300px; max-width:300px width:auto">') 
-    d3.select("#address")
-      .text(d.Address)
-    d3.select("#olderNames")
-      //.text("Previous Names:  "+ d.OlderNames) 
-     .text(d.OlderNames === null ? "": "Previous Names: "+d.OlderNames)
-    /*   .text(`${
-        function(d){
-        if(d.OlderNames ===! null){
-          return "Previous Names: "+d.OlderNames}
-        } }`  ) */
-    // .style("visibility",d=>d.OlderNames === null ? "hidden": "visible")
-     //.text(d=>d.OlderNames === null ? "": "Previous Names: "+`${d.OlderNames}`)
-    d3.select("#year")
-      .text("Active since:  "+d.Year)
-    d3.select("#capacity")       
-      .text("Capacity: "+formatNumber(d.Capacity))
-    d3.select("#shows")       
-  //.text("Notable Shows: "+d.NotableShows)
-      .text(d.NotableShows === null ? "": "Previous Names: "+d.NotableShows)
-    d3.select("#theaterDetailsButton") 
-          .html('<button class="filter-buttons"><a style="text-decoration: none;target="_new"; href=' + d.WebLink + '>Learn More</a></button>')
-    //Show the tooltip
-    d3.select("#part1-tooltip").classed("hidden", false);
-    })
-
+    // Drawing the circles
+    
+    let dots=maparea
+      .selectAll(".circle")
+      .data(state.theaters,d=>`${d.No}_${d.Name}`)
+      .join("circle")
+      .attr("class","dot")
+      .attr("r", 4)
+      .attr("fill", d => d.Type==="Broadway"?"#DC756D":"#017a96")
+      .attr("fill-opacity",0.5)
+      .attr("stroke-width","1")
+      .attr("stroke",d => d.Type==="Broadway"?"#974e49":"#026075")
+      .attr("transform", d => {
+        const [x, y] = projection([d.Longitude, d.Latitude]);
+        return `translate(${x}, ${y})`;
+      })
+      .attr ("r", d => rScale(d.Capacity))
+    
+    //Populate details for static tooltip - it works both with mouseover and click, in order to keep it
+    dots.on("mouseover click", function(d) {                                                            
+        console.log(d3.event.pageX)
+        d3.select("#part1-tooltip")					
+          .select("#tooltipheader")
+          .text(d.Name)  
+        d3.select("#type")
+          .text(d.Type.toUpperCase())  
+          .style("color", d.Type==="Broadway"?"#DC756D":"#017a96")  
+        d3.select("#image-theater")
+          .html('<img src="' + d.ImageLink+ '" style="max-height:300px; max-width:300px width:auto">') 
+        d3.select("#address")
+          .text(d.Address)
+          .style("color", "#6d6d6d") 
+        d3.select("#previous")
+        .style("color", "#6d6d6d") 
+        .text(d.OlderNames === null ? "": "Previous Names: ") 
+        d3.select("#oldernamesvalue")
+        .style("color", "#6d6d6d") 
+        .text(d.OlderNames === null ? "": d.OlderNames)
+        d3.select("#year")
+          .text("Active since "+d.Year)
+          .style("color", "grey") 
+        d3.select("#capacity")       
+          .text("Capacity: "+formatNumber(d.Capacity))
+          .style("color", d.Type==="Broadway"?"#DC756D":"#017a96")  
+        d3.select("#noteable")       
+          .text(d.NotableShows === null ? "":"Noteable Shows: ")
+          .style("color", "grey") 
+        d3.select("#shownames")       
+          .text(d.NotableShows === null ? "":d.NotableShows)
+        d3.select("#theaterDetailsButton") 
+            .html('<button><a target="_blank", style="text-decoration: none", href=' + d.WebLink + '>Learn More</a></button>')
+        d3.select(this)
+            .transition()
+            .duration('50')
+            .attr('fill-opacity', '1')
+            .attr("stroke-width","1.5")
+            .attr ("r", d => rScale(d.Capacity)*1.3)
+      })
+      .on("mouseleave", function(d) {
+        d3.select(this).transition()
+            .duration('50')
+            .attr('fill-opacity', '0.5')
+            .attr("stroke-width","1")
+            .attr ("r", d => rScale(d.Capacity))          
+      })  
+  
   }
 }
--- Narrative-Broadway/timeline.js
+++ Narrative-Broadway/timeline.js
@@ -18,7 +18,6 @@ export function Timeline() {
 
   /**
    * LOAD DATA
-   * Using a Promise.all([]), we can load more than one dataset at a time
    * */
 d3.csv("./data/Longest_Running_Shows_v2020-06-02.csv", d3.autoType) 
   .then(raw_data => {
@@ -41,6 +40,7 @@ d3.csv("./data/Longest_Running_Shows_v2020-06-02.csv", d3.autoType)
         draw(); 
       });
 
+ //Populate dropdown options
     selectBorough
         .selectAll("option")
         .data(["Longest Running","Most Recent","Most Number of Performances"])
@@ -51,20 +51,22 @@ d3.csv("./data/Longest_Running_Shows_v2020-06-02.csv", d3.autoType)
       showsdata=state.shows
       console.log(showsdata)
 
-      xScale = d3
+  //Create scales and axes functions
+    xScale = d3
       .scaleTime()
       .domain([d3.min(showsdata, d => d.OpeningDate),d3.max(showsdata, d => d.ClosingDate)])
       .range([margin.left, width - margin.right])
 
-      yScale = d3
+    yScale = d3
       .scaleBand()
       .domain(showsdata.map(d => d.ShowName))
       .range([height - margin.bottom, margin.top])
       .paddingInner(paddingInner);
 
-      xAxis = d3.axisBottom(xScale);
-      yAxis = d3.axisLeft(yScale);
+    xAxis = d3.axisBottom(xScale);
+    yAxis = d3.axisLeft(yScale);
 
+  //Create svg and axes
     svg = d3
       .select("#part2-timeline")
       .append("svg")
@@ -88,27 +90,31 @@ d3.csv("./data/Longest_Running_Shows_v2020-06-02.csv", d3.autoType)
 
   /**
    * DRAW FUNCTION
-   * we call this everytime there is an update to the data/state
    * */
   function draw() {
-  let formatNumber = d3.format(",")
+    let formatNumber = d3.format(",")
 
-  state.shows
-    .sort((a, b) => {
-    if (state.orderby === "Most Recent") {
-        return d3.ascending(a.ClosingDate, b.ClosingDate)
-    } else if (state.orderby === "Longest Running") {
-    return d3.ascending(+a.Years, +b.Years)
-    } else if (state.orderby === "Most Number of Performances")
-    return d3.ascending(+a.NumberofPerformances, +b.NumberofPerformances)
-    }); 
-  yScale.domain(showsdata.map(d => d.ShowName))
-  d3.select("g.y-axis")
-    .transition()
-    .duration(1000)
-    .call(yAxis.scale(yScale))
-
-  const bars = svg
+  //Sort data based on selection option
+    state.shows
+      .sort((a, b) => {
+      if (state.orderby === "Most Recent") {
+          return d3.ascending(a.ClosingDate, b.ClosingDate)
+      } else if (state.orderby === "Longest Running") {
+      return d3.ascending(+a.Years, +b.Years)
+      } else if (state.orderby === "Most Number of Performances")
+      return d3.ascending(+a.NumberofPerformances, +b.NumberofPerformances)
+      }); 
+  
+  //Update axes for new order  
+    yScale.domain(showsdata.map(d => d.ShowName))
+    
+    d3.select("g.y-axis")
+      .transition()
+      .duration(1000)
+      .call(yAxis.scale(yScale))
+  
+  //Draw thick bars which are the actual gantt periods
+    const bars = svg
       .selectAll("g.bar")
       .data(showsdata,d=>`${d.ShowName}`)
       .join(
@@ -118,110 +124,154 @@ d3.csv("./data/Longest_Running_Shows_v2020-06-02.csv", d3.autoType)
             .attr("class", "bar")
             .call(enter => enter.append("rect"))
             .call(enter => enter.append("text")),
-        update => update,
-        exit => exit.remove()
-      )
+          update => update,
+          exit => exit.remove()
+        )
 
-  bars
-    .transition()
-    .duration(duration)
-    .attr(
-      "transform",
-      d => `translate(${xScale(d.OpeningDate)}, ${yScale(d.ShowName)})`
-    );
+    bars
+      .transition()
+      .duration(duration)
+      .attr(
+        "transform",
+        d => `translate(${xScale(d.OpeningDate)}, ${yScale(d.ShowName)})`
+      );
 
-  bars
-    .select("rect")
-    .transition()
-    .ease(d3.easeExpIn)
-    .duration(duration)
-    .attr("width", d => xScale(d.ClosingDate)-xScale(d.OpeningDate))
-    .attr("height", yScale.bandwidth())
-    .style("fill", "#DC756D")
-    //.style("fill", d => d.metric === state.selectedMetric ? "purple" : "#ccc")
+    bars
+      .select("rect")
+      .transition()
+      .ease(d3.easeExpIn)
+      .duration(duration)
+      .attr("width", d => xScale(d.ClosingDate)-xScale(d.OpeningDate))
+      .attr("height", yScale.bandwidth())
+      .style("fill", "#DC756D")
+      //.style("fill", d => d.metric === state.selectedMetric ? "purple" : "#ccc")
 
-  bars
-    .select("text")
-    .attr("dy", "-.5em")
-  //  .text(d => `${d.metric}: ${this.format(d.value)}`);
+    bars
+      .on("mouseover", function(d) {                                                            
+      
+        /*All the below 5 attempts are to try to position tooltip dynamically
+        */
+        
+        
+        /* 
+        //Attempt 1: Trying to figure out rule of coordinate based quadrants to generate 4 conditions
+        let x = d3.event.pageX - document.getElementById("part2-timeline").getBoundingClientRect().x + 10
+        let y = d3.event.pageY - document.getElementById("part2-timeline").getBoundingClientRect().y + 10  
+          
+        let xsvg=document.getElementById("part2-timeline").getBoundingClientRect().x
+        let ysvg=document.getElementById("part2-timeline").getBoundingClientRect().y
+        console.log("height:",height, ", width:",width,", d3.event.pageX:",d3.event.pageX,", d3.event.pageY:",d3.event.pageY,"x",x,"y",y,"xsvg",xsvg,"ysvg",ysvg) 
+          */
 
-  bars
-     .on("mouseover", function(d) {                                                            
-      //console.log(d3.event.pageX)
-/*       d3.select("#tooltip")
-              .style("left", (d3.event.pageX-100)  + "px")
-              .style("top", d3.event.pageY + "px")						
-              .select("#events")
-              .text(d.Events)
-              .style("fill", "white") */
-     // bars.style("fill", "white")
-      d3.select(this).transition()
-        .duration('50')
-        .attr('opacity', '.4')
-      d3.select("#tooltip2")     
-        .style("left", (d3.event.pageX)  + "px")
-        .style("top", d3.event.pageY + "px")	  
-        .select("#tooltipheader2")
-        .text(d.ShowName)
-      d3.select("#poster")
-        .html('<img src="' + d.ImageLink+ '" style="max-height:200px; width:auto">') 
-      d3.select("#showtype")
-        .text(d.ShowType)
-      d3.select("#opening")
-        .text(d3.timeFormat("%B %d, %Y")(d.OpeningDate))
-      d3.select("#closing")
-        .text(d3.timeFormat("%B %d, %Y")(d.ClosingDate))      
-      //  .text(d3.timeFormat("%B %d, %Y")(d.ClosingDateOriginal))
-      d3.select("#performances")
-        .text(formatNumber(d.NumberofPerformances))
-       // .text(d.NumberofPerformances)
-      d3.select("#years")
-        .text(d.Years)
-      d3.select("#synopsis")
-        .text(d.Synopsis)
-      d3.select("#tooltip2").classed("hidden", false);
-      })  
-    .on("mouseleave", function(d) {
+        /*  
+        //Attempt 2: Tyring to write 2 conditions to use in .style("top") or ("bottom")
+        let mouseposition=function(d){
+              if {d3.event.pageX>2000} {
+                return "bottom"
+              } else {return "top"}
+            }
+          console.log("mosueposition",mouseposition)
+          let tooltipshift=function(d){
+              if (d3.event.pageX>2000) {
+                return height
+              } else {return 0}
+            }
+        */
+        
+        /*
+        //Attempt 3: Even simpler to use 2 conditions, to chage when it is at the bottom
+        let y;
+        if (d3.event.pageY <2000) return {y:d3.event.pageY+100}
+        else if (d3.event.pageY >=2000) return {y:d3.event.pageY}
+        console.log("y",y)
+        */
+
+        /*       
+        //Attempt 4: Trying to write the conditions in style  
+        d3.select("#tooltip2")     
+          .style("left", (d3.event.pageX)  + "px")
+          //.style("top", d3.event.pageY + "px")
+          .style(call(
+            if (d3.event.pageY>2000) return {"top",d3.event.pageY+"px"}
+          )) */
+
+        /*  Attempt 5: Tried calling different values of top based on if
+        if (d3.event.pageY <2000) return {d3.select("#tooltip2").style("top","1800px")}
+        else if (d3.event.pageY >=2000) return {d3.select("#tooltip2").style("top","2000px")}  */
+
+
+        //This was for the Attempt 2:
+         // .style("${mouseposition}", (d3.event.pageY+10*tooltipshift) + "px")	
+
         d3.select(this).transition()
           .duration('50')
-          .attr('opacity', '1')
-          .style("fill", "steelblue")
-        d3.select("#tooltip2").classed("hidden", true);
-      })  
-  
-  const lines = svg
-      .selectAll("g.thinbar")
-      .data(showsdata,d=>`${d.ShowName}`)
-      .join(
-        enter =>
-          enter
-            .append("g")
-            .attr("class", "thinbar")
-            .call(enter => enter.append("rect")),
-        update => update,
-        exit => exit.remove()
-      )
+          .attr('opacity', '.4')
 
-  lines
-    .transition()
-    .duration(duration)
-    .attr(
-      "transform",
-      d => `translate(${margin.left}, ${yScale(d.ShowName)+yScale.bandwidth()/2})`
-    );
-
-  lines
-    .select("rect")
-    .transition()
-    .ease(d3.easeExpIn)
-    .duration(duration)
-    .attr("width", d => xScale(d.OpeningDate)-margin.left)
-    .attr("height", 0.3)
-    .style("fill", "#974e49")
-    .style("fill-opacity", 1)
-
-
+        d3.select("#tooltip2")     
+          .style("left", (d3.event.pageX) + "px")
+          //.style("top", y + "px")    //Removed to make this not dynamic, which is at least not going down
+        
+         
+        d3.select("#tooltipheader2")
+          .text(d.ShowName)
+        d3.select("#poster")
+          .html('<img src="' + d.ImageLink+ '" style="max-height:200px; width:auto">') 
+        d3.select("#showtype")
+          .text(d.ShowType)
+        d3.select("#opening")
+          .text(d3.timeFormat("%B %d, %Y")(d.OpeningDate))
+        d3.select("#closing")
+          .text(d.ClosingDateOriginal==="Present"? "Present":d3.timeFormat("%B %d, %Y")(d.ClosingDateOriginal))
+          .classed("emphasizedtext",d.ClosingDateOriginal==="Present"? true : false)
+        d3.select("#performances")
+          .text(formatNumber(d.NumberofPerformances))
+        d3.select("#years")
+          .text(d.Years)
+        d3.select("#synopsis")
+          .text(d.Synopsis)
+        d3.select("#tooltip2").classed("hidden", false);
+        })  
+      .on("mouseleave", function(d) {
+          d3.select(this).transition()
+            .duration('50')
+            .attr('opacity', '1')
+            .style("fill", "steelblue")
+          d3.select("#tooltip2").classed("hidden", true);
+        })  
     
+    const lines = svg
+        .selectAll("g.thinbar")
+        .data(showsdata,d=>`${d.ShowName}`)
+        .join(
+          enter =>
+            enter
+              .append("g")
+              .attr("class", "thinbar")
+              .call(enter => enter.append("rect")),
+          update => update,
+          exit => exit.remove()
+        )
+
+    lines
+      .transition()
+      .duration(duration)
+      .attr(
+        "transform",
+        d => `translate(${margin.left}, ${yScale(d.ShowName)+yScale.bandwidth()/2})`
+      );
+
+    lines
+      .select("rect")
+      .transition()
+      .ease(d3.easeExpIn)
+      .duration(duration)
+      .attr("width", d => xScale(d.OpeningDate)-margin.left)
+      .attr("height", 0.3)
+      .style("fill", "#974e49")
+      .style("fill-opacity", 1)
+
+
+      
 
   }
 }
